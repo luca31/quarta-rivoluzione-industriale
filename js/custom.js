fdef1e9c45567eae8a035cd20392a2d6224fd27a
Luca Colli <35486975+luca31@users.noreply.github.com>
2022-03-02 07:32:29 +0000

    devs

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,15 @@ $(function () {
 	}, 500);
 
 });
+
+const changeColor = () => {
+  if($(".text-bg span").css("color") == "#ffdd00" || $(".text-bg span").css("color") == "rgb(255, 221, 0)") {
+    $(".banner_main").css("background-image","linear-gradient(290deg, #0d67dc 50%, #08be88 100%)");
+    $(".text-bg span").css("color", "#08e8a6");
+    $(".text-bg p").css("color", "#fff");
+  } else {
+    $(".banner_main").css("background-image","linear-gradient(#0057b7 43%, #ffdd00 0%)");
+    $(".text-bg span").css("color", "#ffdd00");
+    $(".text-bg p").css("color", "#0057b7");
+  }
+}
